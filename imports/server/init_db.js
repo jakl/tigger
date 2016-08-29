51b6cce2edbfef9b3616fdebd05c20bff2f0203a
James Koval <jkoval@twitter.com>
2016-08-29 16:40:55 -0700

    WIP: Porting to new files collection

--- imports/server/init_db.js
+++ imports/server/init_db.js
@@ -7,17 +7,13 @@ Meteor.startup(function() {
   db.Files.remove({});
   db.Tags.remove({});
 
-  // walk.match(util.sharedFilesPath).map(fullPath => {
-  //   const file = path.basename(fullPath);
-  //   const relativePath = util.relative(fullPath);
-  //   const tags = util.getTags(path.dirname(relativePath));
-  // 
-  //   tags.map(util.addTag);
-  // 
-  //   db.Files.insert({
-  //     fileName: file,
-  //     tags: tags,
-  //     file: util.sharedFilesPath + '/' + relativePath
-  //   });
-  // });
+  walk.match(util.sharedFilesPath).map(fullPath => {
+    const file = path.basename(fullPath);
+    const relativePath = util.relative(fullPath);
+    const tags = util.getTags(path.dirname(relativePath));
+
+    tags.map(util.addTag);
+
+    db.Files.addFile(fullPath, { meta: { tags: tags } });
+  });
 });
--- imports/shared/db.js
+++ imports/shared/db.js
@@ -5,6 +5,7 @@ export const Tags = new Meteor.Collection('tags');
 export const Files = new FilesCollection({
   collectionName: 'Files',
   allowClientCode: true, // false to disallow remove files from Client
+  storagePath: process.env.SHARED_PATH || [process.env.PWD, 'public', '.#files'].join('/'),
   // onBeforeUpload: function (file) {
   //   Example allow upload files under 10MB, and only in png/jpg/jpeg formats
   //   if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
